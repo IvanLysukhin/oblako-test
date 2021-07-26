0ecf15207442b90028748cc87c873fa955c7454f
Ivan Lysukhin <ilysukhin@mail.ru>
2021-07-26 15:16:23 +0300

    desktop style

--- src/componets/card/card.jsx
+++ src/componets/card/card.jsx
@@ -7,6 +7,7 @@ function Card() {
                 <p className="card__text card__text--time">23 дек, 9:00</p>
                 <p className="card__text card__text--name">Страхование с заботой о клиенте</p>
                 <p className="card__text card__text--number">12</p>
+                <p className="card__text card__text--coach"></p>
                 <p className="card__text card__text--percent">95 %</p>
             </article>
         </li>
--- src/componets/form/from.jsx
+++ src/componets/form/from.jsx
@@ -5,7 +5,7 @@ function Form() {
         <form className="filter-form" action="#">
             <ul className="filter-form__list">
                 <li className="filter-form__item filter-form__item--search">
-                    <label className="filter-form__label visually-hidden" htmlFor="search">Поиск</label>
+                    <label className="filter-form__label filter-form__label--search visually-hidden" htmlFor="search">Поиск</label>
                     <input className="filter-form__input filter-form__input--search" type="text" id="search" name="search" placeholder="Поиск"/>
                 </li>
                 <li className="filter-form__item filter-form__item--sort">
@@ -18,15 +18,27 @@ function Form() {
                 </li>
                 <li className="filter-form__item">
                     <label className="filter-form__label" htmlFor="program">Программа</label>
-                    <input className="filter-form__input" type="text" id="program" name="program"/>
+                    <select className="filter-form__input" name="program" id="program">
+                        <option className="filter-form__program-option" value="All">Все</option>
+                        <option className="filter-form__program-option" value="Not">Все</option>
+                        <option className="filter-form__program-option" value="Random">Все</option>
+                    </select>
                 </li>
                 <li className="filter-form__item">
-                    <label className="filter-form__label" htmlFor="period">Период</label>
-                    <input className="filter-form__input" type="text" id="period" name="period"/>
+                    <label className="filter-form__label" htmlFor="date">Период</label>
+                    <select className="filter-form__input" name="date" id="date">
+                        <option className="filter-form__program-option" value="June">Июнь 2019</option>
+                        <option className="filter-form__program-option" value="June">Июнь 2019</option>
+                        <option className="filter-form__program-option" value="June">Июнь 2019</option>
+                    </select>
                 </li>
                 <li className="filter-form__item">
                     <label className="filter-form__label" htmlFor="status">Статус</label>
-                    <input className="filter-form__input" type="text" id="status" name="status"/>
+                    <select className="filter-form__input" name="status" id="status">
+                        <option className="filter-form__program-option" value="June">Все</option>
+                        <option className="filter-form__program-option" value="June">Все</option>
+                        <option className="filter-form__program-option" value="June">Все</option>
+                    </select>
                 </li>
             </ul>
             <div className="filter-form__info-container">
